8331bd6b04711b665f95638cf4f65dc8fa67241d
Zinput <jasonzheng2004@gmail.com>
2025-03-31 11:48:49 -0400

    run format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
 };
 
 function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-    <div>Home</div>
+	return (
+		<nav className="flex w-full items-center justify-between border-b p-4 font-semibold text-xl">
+			<div>Home</div>
 
-    <div>Sign In</div>
-    </nav>
-  )
+			<div>Sign In</div>
+		</nav>
+	);
 }
 
 export default function RootLayout({
@@ -24,10 +24,10 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en">
-    <body className={"font-sans ${inter.variable} flex flex-col gap-4"}>
-			<TopNav />
-      {children}
-      </body>
+			<body className={"${inter.variable} flex flex-col gap-4 font-sans"}>
+				<TopNav />
+				{children}
+			</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import Link from "next/link";
 
 const mockUrls = [
-  "https://github.com/CWRUnix/marketing/blob/main/assets/CWRU_Linux_Club_with%20text_tdfs.png?raw=true",
-]
+	"https://github.com/CWRUnix/marketing/blob/main/assets/CWRU_Linux_Club_with%20text_tdfs.png?raw=true",
+];
 
 const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
+	id: index + 1,
+	url,
 }));
 
 export default function HomePage() {
-  return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt="image" />
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+	return (
+		<main className="">
+			<div className="flex flex-wrap gap-4">
+				{[...mockImages, ...mockImages, ...mockImages].map((image) => (
+					<div key={image.id} className="w-48">
+						<img src={image.url} alt="mock" />
+					</div>
+				))}
+			</div>
+		</main>
+	);
 }
